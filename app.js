afa3172d9bef6674acfe9b497eb8a5456c1da286
Sabrina Poderis <sabrinapoderis@gmail.com>
2020-06-09 12:21:13 -0300

    Adicionando Rotas

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const handlebars    = require('express-handlebars')
 const bodyParser    = require('body-parser');
 // const mongoose      = require('mongoose');
 const app           = express();
+const admin         = require('./routes/admin');
 
 // Configurações
-// Body Parser
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(bodyParser.json());
-// Handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
-// Mongoose
-    // Em breve
-// Rotas
-    app.get('/', (req, res) => {
-        res.send('Rota principal');
-    });
-// Outros
-const PORT = 8081;
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    // Mongoose
+        // Em Breve
+    // Rotas
+        app.get('/', (req, res) => {
+            res.send('Rota principal');
+        });
+        app.use('/admin', admin);
+    // Outros
+    const PORT = 8081;
+
 app.listen(PORT, () => {
     console.log('Servidor ligado! http://localhost:'+PORT+'/');
 });
