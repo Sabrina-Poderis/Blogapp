4779b168c75e895f0f632c390aa5c7b33bbb1593
Sabrina Poderis <sabrinapoderis@gmail.com>
2020-06-12 19:25:47 -0300

    Listando Categorias

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,4 @@
 require('../models/Categoria');
-
 const express   = require('express');
 const router    = require('express').Router();
 const mongoose  = require('mongoose');
@@ -14,7 +13,12 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render('../views/layouts/admin/categorias');
+    Categoria.find().sort({data:'desc'}).then((categorias) => {
+        res.render('../views/layouts/admin/categorias', {categorias: categorias.map(categorias => categorias.toJSON())})
+    }).catch((erro) => {
+        req.flash('error_msg', 'Ocorreu um erro!' + erro);
+        res.redirect('/admin');
+    })
 });
 
 router.get('/categorias/add', (req, res) => {
@@ -22,16 +26,36 @@ router.get('/categorias/add', (req, res) => {
 });
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
-    };
+    var erros = []
+    
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: 'Nome inválido'});
+    } else if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"});
+    }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria salva com sucesso!');
-    }).catch((erro) => {
-        console.log('Erro ao salvar categoria!' + erro);
-    });
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: 'Slug inválido'});
+    }
+
+    if(erros.length > 0){
+        res.render('../views/layouts/admin/add-categorias', {erros: erros});
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        };
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria salva com sucesso!');
+            res.redirect('/admin/categorias');
+        }).catch((erro) => {
+            req.flash('error_msg', 'Ocorreu um erro!' + erro);
+            res.redirect('/admin');
+        });
+    }
+
+    
 });
 
 module.exports = router;
