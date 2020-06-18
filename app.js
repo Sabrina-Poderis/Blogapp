91f96e21d0a8ba9940f8506e888518cd490c0029
Sabrina Poderis <sabrinapoderis@gmail.com>
2020-06-18 18:23:36 -0300

    Adicionada a página de categorias

--- app.js
+++ app.js
@@ -74,6 +74,36 @@
                 res.redirect("/")
             })
         });
+
+        app.get('/categorias', (req, res) => {
+            Categoria.find().sort({nome: 'asc'}).then((categorias) => {
+                res.render('../views/layouts/categorias/index', {categorias: categorias.map(categorias => categorias.toJSON())})
+            }).catch((erro) => {
+                req.flash('error_msg', 'Ocorreu um erro!' + erro);
+                res.redirect('/admin');
+            });
+        })
+
+        app.get('/categorias/:slug', (req, res) => {
+            Categoria.findOne({slug:req.params.slug}).lean().then((categoria) => {
+                if(categoria){
+                    Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                        res.render('../views/layouts/categorias/postagens', {postagens: postagens, categoria: categoria});
+                        console.log(postagens);
+                    }).catch((erro) => {
+                        req.flash('error_msg', 'Erro! Não foi possível listar os posts');
+                        res.redirect('/');
+                    })
+                } else {
+                    req.flash('error_msg', 'Erro! Esta categoria não existe');
+                    res.redirect('/');
+                }
+            }).catch(() => {
+                req.flash('error_msg', 'Erro interno ao carregar esta categoria');
+                res.redirect('/');
+            })
+        });
+        
         app.use('/admin', admin);
     // Outros
         const PORT = 8081;
