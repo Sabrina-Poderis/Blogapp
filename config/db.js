b6fb052970fe43f0f00209112c19e3372a98d423
Sabrina-Poderis <sabrina.poderis@gmail.com>
2020-06-29 02:23:35 -0300

    Removendo node_modules

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
     module.exports = {
-        mongoURI: "mongodb+srv:[...]"
+        mongoURI: "mongodb+srv://varphy:PmtSIi64cSdEcycY@blogapp-prod-i4pfd.mongodb.net/blogapp-prod?retryWrites=true&w=majority"
     }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/blogapp" }
